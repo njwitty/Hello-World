9a83694b59931dae3a5bb223cecbd3b0f046c097
Nabajyoti <nababaruah@gmail.com>
2023-01-04 20:07:07 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+// deploy.js
+
+async function main() {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+
+    const hello_world = await HelloWorld.deploy("Hello World!");
+    console.log("Contract deployed to address: ", hello_world.address);
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
